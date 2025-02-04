d58bfbde031c32e0849a0779ce89b1ff6924ca3a
Kabir Dulal <zamfy_fade21@yahoo.com>
2025-02-04 11:10:10 +1100

    added leader team functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let homeScore = 0;
 let guestScore = 0;
+let leaderPoint = 0;
 
 function increaseScore(team, points) {
     if (team === "home") {
@@ -8,5 +9,22 @@ function increaseScore(team, points) {
     } else if (team === "guest") {
         guestScore += points;
         document.getElementById("guest-score").textContent = guestScore;
+    }
+    if(homeScore>guestScore){
+       /* let teamEl = document.getElementById("team-el");
+        teamEl.textContent = "HOME"; */
+        leaderPoint = homeScore - guestScore;
+        let pointEl = document.getElementById("point-el");
+        pointEl.textContent = "Home is Leading By: " + leaderPoint + " Points";
+    }
+    else if(guestScore>homeScore){
+        leaderPoint = guestScore - homeScore;
+        let pointEl = document.getElementById("point-el");
+        pointEl.textContent = "AWAY is Leading By: " + leaderPoint + " Points";
+
+    }
+    else if(homeScore === guestScore){
+        let pointEl = document.getElementById("point-el");
+        pointEl.textContent = "Scores are EQUAL";
     }
 }
